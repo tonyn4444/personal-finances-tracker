9ffc0508b2b4f30271a0d9c5ddb0af6f8692133d
Tony Nguyen <tntuyen@ucdavis.edu>
2017-01-18 16:12:39 -0800

    add seeds, Record resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  Record.create title: 'Record 1', date: Date.today, amount: 500
+  Record.create title: 'Record 2', date: Date.today, amount: -100
