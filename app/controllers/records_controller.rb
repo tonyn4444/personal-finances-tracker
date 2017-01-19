3c6b73dfb289e8cf2eb98c118e20d8162aad85b7
Tony Nguyen <tntuyen@ucdavis.edu>
2017-01-19 11:47:15 -0800

    add delete and edit methods, convert coffee script to jsx, change home route

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,22 +1,40 @@
 class RecordsController < ApplicationController
-	def index
-		@records = Record.all
-	end
+  before_action :current_record, only: [:update, :destroy]
+  
+  def index
+    @records = Record.all
+  end
 
-	def create
-		@record = Record.new(record_params)
+  def create
+    @record = Record.new(record_params)
 
-		if @record.save
-			render json: @record
-		else
-			render json: @record.errors, status: :unprocessable_entity
-		end
-	end
+    if @record.save
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
 
-	private
+  def update
+    if @record.update(record_params)
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
 
-		def record_params
-			params.require(:record).permit(:title, :amount, :date)
-		end
+  def destroy
+    @record.destroy
+    head :no_content
+  end
+
+  private
+  
+    def record_params
+      params.require(:record).permit(:title, :amount, :date)
+    end
+
+    def current_record
+      @record = Record.find(params[:id])
+    end
 end
-
